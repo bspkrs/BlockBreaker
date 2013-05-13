a837c8852f53ee6e893e9ef95bcab858b9751da2
bspkrs <bspkrs@gmail.com>
2013-05-13 01:33:18 -0400

    fixed config setup

--- src/bspkrs/blockbreaker/BBSettings.java
+++ src/bspkrs/blockbreaker/BBSettings.java
@@ -2,7 +2,6 @@ package bspkrs.blockbreaker;
 
 import java.io.File;
 
-import bspkrs.fml.util.Config;
 import bspkrs.util.CommonUtils;
 import bspkrs.util.Configuration;
 
@@ -17,21 +16,21 @@ public final class BBSettings
     public static Configuration config;
     
     // Config file fields
-    public final static String  blockListDesc      = "Block List, \";\" splits blocks and \",\" splits block ID and metadata.";
-    public static String        blockList          = "17;";
-    public final static String  itemDropModeDesc   = "Item drops values: 0-No drops. 1-Drops in Survival. 2-Drops in Survival & Creative. 3-Drops in Survival, Creative, & Adventure";
-    public static int           itemDropMode       = 1;
-    public final static String  blockLimitDesc     = "Limit of blocks to be destroyed at once. Use -1 for infinite.";
-    public static int           blockLimit         = 800;
-    public final static String  maxDistanceDesc    = "Maximum distance from the first block to search for blocks to destroy. Use -1 for infinite.";
-    public static int           maxDistance        = 6;
-    public final static String  blocksPerTickDesc  = "Maximum number of blocks to be removed per game tick (1/20 seconds). Using a low number will keep the game from getting huge" +
-                                                           "performance drops but also decreases the speed at which blocks are destroyed.";
-    public static int           blocksPerTick      = 50;
-    public final static String  sneakActionDesc    = "Set sneakAction = \"disable\" to disable the block breaker effect while sneaking,\n" +
-                                                           "set sneakAction = \"enable\" to only enable the block breaker effect while sneaking,\n" +
-                                                           "set sneakAction = \"none\" to have the block breaker effect enabled regardless of sneaking.";
-    public static String        sneakAction        = "disable";
+    public final static String  blockListDesc     = "Block List, \";\" splits blocks and \",\" splits block ID and metadata.";
+    public static String        blockList         = "17;";
+    public final static String  itemDropModeDesc  = "Item drops values: 0-No drops. 1-Drops in Survival. 2-Drops in Survival & Creative. 3-Drops in Survival, Creative, & Adventure";
+    public static int           itemDropMode      = 1;
+    public final static String  blockLimitDesc    = "Limit of blocks to be destroyed at once. Use -1 for infinite.";
+    public static int           blockLimit        = 800;
+    public final static String  maxDistanceDesc   = "Maximum distance from the first block to search for blocks to destroy. Use -1 for infinite.";
+    public static int           maxDistance       = 6;
+    public final static String  blocksPerTickDesc = "Maximum number of blocks to be removed per game tick (1/20 seconds). Using a low number will keep the game from getting huge" +
+                                                          "performance drops but also decreases the speed at which blocks are destroyed.";
+    public static int           blocksPerTick     = 50;
+    public final static String  sneakActionDesc   = "Set sneakAction = \"disable\" to disable the block breaker effect while sneaking,\n" +
+                                                          "set sneakAction = \"enable\" to only enable the block breaker effect while sneaking,\n" +
+                                                          "set sneakAction = \"none\" to have the block breaker effect enabled regardless of sneaking.";
+    public static String        sneakAction       = "disable";
     
     public static void loadConfig(File file)
     {
@@ -66,12 +65,12 @@ public final class BBSettings
         
         config.load();
         
-        blockList = Config.getString(config, "blockList", ctgyGen, blockList, blockListDesc);
-        itemDropMode = Config.getInt(config, "itemDropMode", ctgyGen, itemDropMode, 0, 3, itemDropModeDesc);
-        blockLimit = Config.getInt(config, "blockLimit", ctgyGen, blockLimit, -1, Integer.MAX_VALUE, blockLimitDesc);
-        maxDistance = Config.getInt(config, "maxDistance", ctgyGen, maxDistance, -1, 1000, maxDistanceDesc);
-        blocksPerTick = Config.getInt(config, "blocksPerTick", ctgyGen, blocksPerTick, 1, 1000, blocksPerTickDesc);
-        sneakAction = Config.getString(config, "sneakAction", ctgyGen, sneakAction, sneakActionDesc);
+        blockList = config.getString("blockList", ctgyGen, blockList, blockListDesc);
+        itemDropMode = config.getInt("itemDropMode", ctgyGen, itemDropMode, 0, 3, itemDropModeDesc);
+        blockLimit = config.getInt("blockLimit", ctgyGen, blockLimit, -1, Integer.MAX_VALUE, blockLimitDesc);
+        maxDistance = config.getInt("maxDistance", ctgyGen, maxDistance, -1, 1000, maxDistanceDesc);
+        blocksPerTick = config.getInt("blocksPerTick", ctgyGen, blocksPerTick, 1, 1000, blocksPerTickDesc);
+        sneakAction = config.getString("sneakAction", ctgyGen, sneakAction, sneakActionDesc);
         
         config.save();
         
